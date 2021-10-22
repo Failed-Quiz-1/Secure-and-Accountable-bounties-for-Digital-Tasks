9ae4cdd504c7290cb64d5e02b5be58763bdf316f
irvintingsieze <irvintingg@gmail.com>
2021-10-22 15:43:27 +0800

    create tables

--- packages/backend/fiver/src/app.module.ts
+++ packages/backend/fiver/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DraftModule } from './draft/draft.module';
+import { TaskModule } from './task/task.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, DraftModule, TaskModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
