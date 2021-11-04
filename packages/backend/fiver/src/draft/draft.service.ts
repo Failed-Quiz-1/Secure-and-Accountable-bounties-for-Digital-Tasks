381b35918e9471ef1ab39221ea3c9b912e4e523b
sweesenkoh <sweesenworkstation@gmail.com>
2021-11-04 16:03:38 +0800

    fix create draft bug

--- packages/backend/fiver/src/draft/draft.service.ts
+++ packages/backend/fiver/src/draft/draft.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from 'src/task/entities/task.entity';
 import { Users } from 'src/users/entities/user.entity';
@@ -10,7 +15,6 @@ import * as crypto from 'crypto-helper';
 import { SignatureMessage } from 'crypto-helper';
 @Injectable()
 export class DraftService {
-
   constructor(
     @InjectRepository(Users)
     private usersRepository: Repository<Users>,
@@ -19,97 +23,116 @@ export class DraftService {
     @InjectRepository(Draft)
     private draftRepository: Repository<Draft>,
   ) {}
-  
-  async getCurrDateTime (){
-    const currentdate = new Date(); 
-    const datetime = "Last Sync: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
+
+  async getCurrDateTime() {
+    const currentdate = new Date();
+    const datetime =
+      'Last Sync: ' +
+      currentdate.getDate() +
+      '/' +
+      (currentdate.getMonth() + 1) +
+      '/' +
+      currentdate.getFullYear() +
+      ' @ ' +
+      currentdate.getHours() +
+      ':' +
+      currentdate.getMinutes() +
+      ':' +
+      currentdate.getSeconds();
     return datetime;
   }
   async create(createDraftDto: CreateDraftDto) {
-      const newDraft = new Draft();
-      const user = await this.usersRepository.findOne({id:createDraftDto.userid});
-      const task = await this.taskRepository.findOne({id:createDraftDto.taskid});
-      newDraft.author = user;
-      newDraft.task = task;
-      const ppk = crypto.generatePublicAndPrivateKey(createDraftDto.mnemonic);
-      const posterid = task.poster.id;
-      const newMessage : SignatureMessage = {
-        fromUserId: createDraftDto.userid,
-        toUserId: posterid,
-        taskId: createDraftDto.taskid,
-        createdOn: await this.getCurrDateTime(),
-        status: "POSTED",
-      }
-      const draft_msg = JSON.stringify(newMessage);
-      const draft_signature = crypto.createSignature(newMessage, ppk.privateKey)
-      newDraft.draft_signature = draft_signature;
-      newDraft.draft_sig_message = draft_msg;
+    const newDraft = new Draft();
+    const user = await this.usersRepository.findOne({
+      id: createDraftDto.userid,
+    });
+    const task = await this.taskRepository.findOne({
+      where: [{ id: createDraftDto.taskid }],
+      relations: ['poster'],
+    });
+    newDraft.author = user;
+    newDraft.task = task;
+    const ppk = crypto.generatePublicAndPrivateKey(createDraftDto.mnemonic);
+    const posterid = task.poster.id;
+    const newMessage: SignatureMessage = {
+      fromUserId: createDraftDto.userid,
+      toUserId: posterid,
+      taskId: createDraftDto.taskid,
+      createdOn: await this.getCurrDateTime(),
+      status: 'POSTED',
+    };
+    const draft_msg = JSON.stringify(newMessage);
+    const draft_signature = crypto.createSignature(newMessage, ppk.privateKey);
+    newDraft.draft_signature = draft_signature;
+    newDraft.draft_sig_message = draft_msg;
 
-      const isCorrectMnemonic = crypto.verifySignature(newMessage,draft_signature,ppk.publicKey);
-      if (isCorrectMnemonic){
-        await this.draftRepository.save([newDraft]);
-        return newDraft;
-      }
-      throw new BadRequestException("Incorrect mnemonic string!")
+    const isCorrectMnemonic = crypto.verifySignature(
+      newMessage,
+      draft_signature,
+      ppk.publicKey,
+    );
+    if (isCorrectMnemonic) {
+      await this.draftRepository.save([newDraft]);
+      return newDraft;
+    }
+    throw new BadRequestException('Incorrect mnemonic string!');
   }
 
   async update(id: number, rejectDraftDto: RejectDraftDto) {
-      const draft = await this.draftRepository.findOneOrFail({
-        where: [{ id: id }],
-        relations: ['author','task'],
-      });
-      const newMessage:SignatureMessage={
-        fromUserId:draft.task.poster.id,
-        toUserId:draft.author.id,
-        taskId:draft.task.id,
-        createdOn: await this.getCurrDateTime(),
-        status: "REJECTED"
-      }
-      const ppk = crypto.generatePublicAndPrivateKey(rejectDraftDto.mnemonic);
-      const reject_msg = JSON.stringify(newMessage);
-      const reject_signature = crypto.createSignature(newMessage, ppk.privateKey)
-      draft.reject_sig_message = reject_msg;
-      draft.reject_signature = reject_signature;
-      const isCorrectMnemonic = crypto.verifySignature(newMessage,reject_signature,ppk.publicKey);
-      if (isCorrectMnemonic){
-        await this.draftRepository.save([draft]);
-        return draft;
-      }
-      throw new BadRequestException("Incorrect mnemonic string!")
+    const draft = await this.draftRepository.findOneOrFail({
+      where: [{ id: id }],
+      relations: ['author', 'task'],
+    });
+    const newMessage: SignatureMessage = {
+      fromUserId: draft.task.poster.id,
+      toUserId: draft.author.id,
+      taskId: draft.task.id,
+      createdOn: await this.getCurrDateTime(),
+      status: 'REJECTED',
+    };
+    const ppk = crypto.generatePublicAndPrivateKey(rejectDraftDto.mnemonic);
+    const reject_msg = JSON.stringify(newMessage);
+    const reject_signature = crypto.createSignature(newMessage, ppk.privateKey);
+    draft.reject_sig_message = reject_msg;
+    draft.reject_signature = reject_signature;
+    const isCorrectMnemonic = crypto.verifySignature(
+      newMessage,
+      reject_signature,
+      ppk.publicKey,
+    );
+    if (isCorrectMnemonic) {
+      await this.draftRepository.save([draft]);
+      return draft;
+    }
+    throw new BadRequestException('Incorrect mnemonic string!');
   }
 
-  async findByTaskId(taskid: number){
-    try{
+  async findByTaskId(taskid: number) {
+    try {
       const task = await this.taskRepository.findOne({
-        id:taskid
+        id: taskid,
       });
       const allDrafts = await this.draftRepository.find({
         where: [{ task: task }],
-        relations: ['author','task'],
+        relations: ['author', 'task'],
       });
       return allDrafts;
-    }catch(e){
+    } catch (e) {
       return new InternalServerErrorException(e);
     }
   }
 
-
-  async findByUserId(userid: number){
-    try{
+  async findByUserId(userid: number) {
+    try {
       const user = await this.usersRepository.findOne({
-        id:userid
+        id: userid,
       });
       const allDrafts = await this.draftRepository.find({
         where: [{ author: user }],
-        relations: ['author','task'],
+        relations: ['author', 'task'],
       });
       return allDrafts;
-    }catch(e){
+    } catch (e) {
       return new InternalServerErrorException(e);
     }
   }
